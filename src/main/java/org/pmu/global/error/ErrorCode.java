095c7cd4f4df878573fe5231ee34a16d84b6807e
sunwoong <sunwoong1122@gmail.com>
2023-08-19 16:12:32 +0900

    [feat] #2 error code 메시지 추가

--- src/main/java/org/pmu/global/error/ErrorCode.java
+++ src/main/java/org/pmu/global/error/ErrorCode.java
@@ -15,32 +15,32 @@ public enum ErrorCode {
     /**
      * 401 Unauthorized
      */
-    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, ""),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "리소스 접근 권한이 없습니다."),
 
     /**
      * 403 Forbidden
      */
-    FORBIDDEN(HttpStatus.FORBIDDEN, ""),
+    FORBIDDEN(HttpStatus.FORBIDDEN, "리소스 접근 권한이 없습니다."),
 
     /**
      * 404 Not Found
      */
-    ENTITY_NOT_FOUND(HttpStatus.NOT_FOUND, ""),
+    ENTITY_NOT_FOUND(HttpStatus.NOT_FOUND, "엔티티를 찾을 수 없습니다."),
 
     /**
      * 405 Method Not Allowed
      */
-    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, ""),
+    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "잘못된 HTTP method 요청입니다."),
 
     /**
      * 409 Conflict
      */
-    CONFLICT(HttpStatus.CONFLICT, ""),
+    CONFLICT(HttpStatus.CONFLICT, "이미 존재하는 리소스입니다."),
 
     /**
      * 500 Internal Server Error
      */
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "");
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류입니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
