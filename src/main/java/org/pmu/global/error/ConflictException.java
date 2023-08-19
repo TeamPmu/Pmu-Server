72d5f2804ee00114ea724ff1280748766a88e93c
sunwoong <sunwoong1122@gmail.com>
2023-08-19 16:07:15 +0900

    [feat] #2 conflict exception 구현

---
+++ src/main/java/org/pmu/global/error/ConflictException.java
@@ -0,0 +1,11 @@
+package org.pmu.global.error;
+
+public class ConflictException extends BusinessException {
+    public ConflictException() {
+        super(ErrorCode.CONFLICT);
+    }
+
+    public ConflictException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+}
