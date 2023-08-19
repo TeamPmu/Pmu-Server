962d573bcffdf7def7be1001ab4ff81812156666
sunwoong <sunwoong1122@gmail.com>
2023-08-19 16:07:39 +0900

    [feat] #2 bad request exception 구현

---
+++ src/main/java/org/pmu/global/error/InvalidValueException.java
@@ -0,0 +1,11 @@
+package org.pmu.global.error;
+
+public class InvalidValueException extends BusinessException {
+    public InvalidValueException() {
+        super(ErrorCode.BAD_REQUEST);
+    }
+
+    public InvalidValueException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+}
