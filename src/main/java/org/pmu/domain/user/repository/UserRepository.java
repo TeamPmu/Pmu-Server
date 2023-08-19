864fe6006054a2fcb1c32cdbe0efff593c933ac9
sunwoong <sunwoong1122@gmail.com>
2023-08-19 18:37:24 +0900

    [feat] #12 유저 조회 query 추가 구현

--- src/main/java/org/pmu/domain/user/repository/UserRepository.java
+++ src/main/java/org/pmu/domain/user/repository/UserRepository.java
@@ -3,10 +3,13 @@ package org.pmu.domain.user.repository;
 import org.pmu.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findUserByNickname(String nickname);
+    List<User> findUsersByNickname(String nickname);
+
+    List<User> findUsersByPlatformId(String platformId);
 
     Optional<User> findUserByPlatformId(String platformId);
 }
