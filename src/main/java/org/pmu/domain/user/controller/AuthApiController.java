5ef007e9ff772230701d057eb43092ca014dcc63
sunwoong <sunwoong1122@gmail.com>
2023-08-19 18:38:05 +0900

    [feat] #12 카카오 로그인 API 구현

--- src/main/java/org/pmu/domain/user/controller/AuthApiController.java
+++ src/main/java/org/pmu/domain/user/controller/AuthApiController.java
@@ -9,16 +9,20 @@ import org.pmu.global.common.SuccessCode;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/user")
 @Controller
 public class AuthApiController {
     private final AuthService authService;
+
+    @GetMapping("/signin")
+    public ResponseEntity<BaseResponse<?>> signUp(@RequestHeader("Authorization") final String token) {
+        final UserAuthResponseDto userAuthResponseDto = authService.signIn(token);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(BaseResponse.of(SuccessCode.CREATED, userAuthResponseDto));
+    }
 
     @PostMapping("/signup")
     public ResponseEntity<BaseResponse<?>> signUp(@RequestHeader("Authorization") final String token,
