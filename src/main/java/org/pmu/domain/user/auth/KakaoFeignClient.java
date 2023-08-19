cbbf83ed6982ad8a993f291a4bef6102059b582b
sunwoong <sunwoong1122@gmail.com>
2023-08-19 17:36:21 +0900

    [feat] #8 kakao feign client 구현

---
+++ src/main/java/org/pmu/domain/user/auth/KakaoFeignClient.java
@@ -0,0 +1,12 @@
+package org.pmu.domain.user.auth;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(name = "kakao-feign-client", url = "https://kapi.kakao.com/")
+public interface KakaoFeignClient {
+    @GetMapping("v1/user/access_token_info")
+    KakaoAccessTokenInfo getKakaoAccessTokenInfo(@RequestHeader("Authorization") String accessToken);
+}
+
