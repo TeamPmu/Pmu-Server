5db34886e7d20ba04df978356f7cdf9b511fe221
sunwoong <sunwoong1122@gmail.com>
2023-09-21 14:52:02 +0900

    [feat] #24 카카오톡 프로필 이미지 조회 기능 구현

--- src/main/java/org/pmu/domain/user/auth/KakaoOAuthProvider.java
+++ src/main/java/org/pmu/domain/user/auth/KakaoOAuthProvider.java
@@ -7,6 +7,8 @@ import org.pmu.global.error.ErrorCode;
 import org.pmu.global.error.UnauthorizedException;
 import org.springframework.stereotype.Component;
 
+import static org.pmu.domain.user.auth.KakaoAccessToken.createKakaoAccessToken;
+
 @Slf4j
 @RequiredArgsConstructor
 @Component
@@ -14,15 +16,31 @@ public class KakaoOAuthProvider {
     private final KakaoFeignClient kakaoFeignClient;
 
     public String getKakaoPlatformId(String accessToken) {
-        KakaoAccessToken kakaoAccessToken = KakaoAccessToken.createKakaoAccessToken(accessToken);
+        KakaoAccessToken kakaoAccessToken = createKakaoAccessToken(accessToken);
         String accessTokenWithTokenType = kakaoAccessToken.getAccessTokenWithTokenType();
         KakaoAccessTokenInfo kakaoAccessTokenInfo = getKakaoAccessTokenInfo(accessTokenWithTokenType);
         return String.valueOf(kakaoAccessTokenInfo.getId());
     }
 
+    public String getKakaoUserProfileImageUrl(String accessToken) {
+        KakaoAccessToken kakaoAccessToken = createKakaoAccessToken(accessToken);
+        String accessTokenWithTokenType = kakaoAccessToken.getAccessTokenWithTokenType();
+        KakaoUserProfile kakaoUserProfile = getKakaoUserProfile(accessTokenWithTokenType);
+        return kakaoUserProfile.getProfileImageURL();
+    }
+
     private KakaoAccessTokenInfo getKakaoAccessTokenInfo(String accessTokenWithTokenType) {
         try {
             return kakaoFeignClient.getKakaoAccessTokenInfo(accessTokenWithTokenType);
+        } catch (FeignException e) {
+            log.error("Feign Exception: ", e);
+            throw new UnauthorizedException(ErrorCode.INVALID_KAKAO_ACCESS_TOKEN);
+        }
+    }
+
+    private KakaoUserProfile getKakaoUserProfile(String accessTokenWithTokenType) {
+        try {
+            return kakaoFeignClient.getKakaoUserProfile(accessTokenWithTokenType);
         } catch (FeignException e) {
             log.error("Feign Exception: ", e);
             throw new UnauthorizedException(ErrorCode.INVALID_KAKAO_ACCESS_TOKEN);
