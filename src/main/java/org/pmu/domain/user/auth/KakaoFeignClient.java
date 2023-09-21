7c1f7f1ae727f0652e6c1813eef76000a517c5d4
sunwoong <sunwoong1122@gmail.com>
2023-09-21 14:50:43 +0900

    [feat] #24 카카오톡 프로필 이미지 조회 API 구현

--- src/main/java/org/pmu/domain/user/auth/KakaoFeignClient.java
+++ src/main/java/org/pmu/domain/user/auth/KakaoFeignClient.java
@@ -8,5 +8,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 public interface KakaoFeignClient {
     @GetMapping("v1/user/access_token_info")
     KakaoAccessTokenInfo getKakaoAccessTokenInfo(@RequestHeader("Authorization") String accessToken);
-}
 
+    @GetMapping("v1/api/talk/profile")
+    KakaoUserProfile getKakaoUserProfile(@RequestHeader("Authorization") String accessToken);
+}
